e5a34d6700183bca4112fd1ad1c0e37a471f7e62
underminethesystem <julian.op@outlook.de>
2016-10-27 18:05:50 +0200

    meinpizza

--- app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
+++ app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
@@ -9,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hallo
-        //this line is new aka second commit/push
-        //this line is new aka third only subdir commit/push
-        //in browser change
-        //asbsd
-        System.out.print("test");
-        System.out.println("hi");
-        System.out.println("hi2");
-        System.out.println("as");
-        System.out.println("Döner");
     }
 }
---
+++ app/src/main/java/com/example/jules/kugelspiel/game/Game.java
@@ -0,0 +1,8 @@
+package com.example.jules.kugelspiel.game;
+
+/**
+ * Created by jules on 27.10.2016.
+ */
+
+public class Game {
+}
