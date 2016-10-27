140cba8ab68aa7a70074eae041b7d6c60dfb82fe
underminethesystem <julian.op@outlook.de>
2016-10-27 10:52:37 +0200

    the very first commit

--- app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
+++ app/src/main/java/com/example/jules/kugelspiel/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this line is new aka second commit/push
+        //this line is new aka third only subdir commit/push
     }
 }
